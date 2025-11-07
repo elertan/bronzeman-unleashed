e075926a7f171d63196a12ebce5461367ed90e7d
elertan <denkievits@gmail.com>
2025-11-07 20:27:47 +0100

    log party room test

--- src/main/java/com.elertan/policies/FaladorPartyRoomPolicy.java
+++ src/main/java/com.elertan/policies/FaladorPartyRoomPolicy.java
@@ -6,6 +6,8 @@ import com.elertan.GameRulesService;
 import com.elertan.PolicyService;
 import com.elertan.models.GameRules;
 import com.google.inject.Singleton;
+import net.runelite.api.Actor;
+import net.runelite.api.MenuEntry;
 import net.runelite.api.events.MenuOptionClicked;
 
 @Singleton
@@ -38,13 +40,33 @@ public class FaladorPartyRoomPolicy extends PolicyBase implements BUPluginLifecy
             return;
         }
         GameRules gameRules = context.getGameRules();
-        if (gameRules == null || !gameRules.isRestrictGroundItems()) {
+        if (gameRules == null || !gameRules.isRestrictFaladorPartyRoomBalloons()) {
             return;
         }
         enforceMenuOptionClicked(event, context);
     }
 
     private void enforceMenuOptionClicked(MenuOptionClicked event, PolicyContext context) {
+        MenuEntry menuEntry = event.getMenuEntry();
+        if (menuEntry != null) {
+            log.info("menu entry: {}", menuEntry.getIdentifier());
+            Actor actor = menuEntry.getActor();
+            if (actor != null) {
+                log.info("actor name: {}", actor.getName());
+            }
+        }
+
+        log.info(
+            "mo: {}, mao: {}, id: {}, target: {}",
+            event.getMenuOption(),
+            event.getMenuAction().ordinal(),
+            event.getId(),
+            event.getMenuTarget()
+        );
+
         String menuOption = event.getMenuOption();
+        if (!menuOption.equalsIgnoreCase("burst")) {
+            return;
+        }
     }
 }
