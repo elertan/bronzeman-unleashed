5b672baf6716ced053f78036d0af1677fd4002e1
Joffewilliam <joffewilliam@gmail.com>
2026-03-19 23:28:27 -0400

    wire ground item menu-entry and quantity-change event handlers

--- src/main/java/com.elertan/BUPlugin.java
+++ src/main/java/com.elertan/BUPlugin.java
@@ -29,7 +29,9 @@ import net.runelite.api.events.CommandExecuted;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.ItemContainerChanged;
 import net.runelite.api.events.ItemDespawned;
+import net.runelite.api.events.ItemQuantityChanged;
 import net.runelite.api.events.ItemSpawned;
+import net.runelite.api.events.MenuEntryAdded;
 import net.runelite.api.events.MenuOptionClicked;
 import net.runelite.api.events.ScriptCallbackEvent;
 import net.runelite.api.events.ScriptPostFired;
@@ -320,6 +322,11 @@ public final class BUPlugin extends Plugin {
     }
 
     @Subscribe
+    public void onMenuEntryAdded(MenuEntryAdded event) {
+        groundItemsPolicy.onMenuEntryAdded(event);
+    }
+
+    @Subscribe
     public void onVarbitChanged(VarbitChanged event) {
         achievementDiaryService.onVarbitChanged(event);
     }
@@ -345,6 +352,11 @@ public final class BUPlugin extends Plugin {
     @Subscribe
     public void onItemDespawned(ItemDespawned event) {
         groundItemsPolicy.onItemDespawned(event);
+    }
+
+    @Subscribe
+    public void onItemQuantityChanged(ItemQuantityChanged event) {
+        groundItemsPolicy.onItemQuantityChanged(event);
     }
 
     @Subscribe
